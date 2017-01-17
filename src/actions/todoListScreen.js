5fa5cc3223bcbb4904f4aaf1ed32534a26e26658
Atticus White <contact@atticuswhite.com>
2017-01-17 17:37:38 -0500

    actions updated

--- src/actions/todoListScreen.js
+++ src/actions/todoListScreen.js
@@ -1,9 +1,14 @@
 import * as ActionTypes from '@actions/types';
 
 export function newTodoTextChanged(text) {
-
+  return {
+    type: ActionTypes.TODO_TEXT_CHANGED,
+    text
+  }
 }
 
 export function toggleCompletedVisibililty() {
-  
+  return {
+    type: ActionTypes.TOGGLE_SHOW_COMPLETED
+  };
 }
--- src/actions/todos.js
+++ src/actions/todos.js
@@ -1,13 +1,31 @@
 import * as ActionTypes from '@actions/types';
 
-export function addTodo(text) {
+function* createIdGenerator () {
+  var id = 2;
+  while (true) {
+    yield ++id;
+  }
+}
+const idGenerator = createIdGenerator();
 
+export function addTodo(text) {
+  return {
+    type: ActionTypes.ADD_TODO,
+    text,
+    id: idGenerator.next().value
+  }
 }
 
 export function removeTodo(id) {
-
+  return {
+    type: ActionTypes.REMOVE_TODO,
+    id
+  };
 }
 
 export function toggleCompleted(id) {
-
+  return {
+    type: ActionTypes.TOGGLE_TODO,
+    id
+  }
 }
